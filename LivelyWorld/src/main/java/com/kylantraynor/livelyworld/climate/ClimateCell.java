9884be0876d59da3b3c168d6f82b068f737213c7
KylanTraynor <kylan2837@gmail.com>
2017-01-08 14:56:26 +0100

    stuff

--- LivelyWorld/src/main/java/com/kylantraynor/livelyworld/climate/ClimateCell.java
+++ LivelyWorld/src/main/java/com/kylantraynor/livelyworld/climate/ClimateCell.java
@@ -1,13 +1,11 @@
 package com.kylantraynor.livelyworld.climate;
 
 import com.kylantraynor.voronoi.VCell;
-import com.kylantraynor.voronoi.VSite;
-import com.kylantraynor.voronoi.Voronoi;
 
 public class ClimateCell extends VCell{
 
-	public ClimateCell(VSite site, Voronoi v) {
-		super(site, v);
+	public ClimateCell() {
+		super();
 	}
 
 }

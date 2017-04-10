c65c45f3484665c2f5be54d86368c2267528e251
KylanTraynor <kylan2837@gmail.com>
2017-04-10 14:06:52 +0200

    Added auto breed for cows and sheeps.

--- LivelyWorld/src/main/java/com/kylantraynor/livelyworld/creatures/CreaturesModule.java
+++ LivelyWorld/src/main/java/com/kylantraynor/livelyworld/creatures/CreaturesModule.java
@@ -3,24 +3,111 @@ package com.kylantraynor.livelyworld.creatures;
 import java.util.logging.Level;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.World;
+import org.bukkit.Chunk;
+import org.bukkit.attribute.Attribute;
 import org.bukkit.block.Biome;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
+import org.bukkit.entity.Ageable;
+import org.bukkit.entity.Cow;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import com.kylantraynor.livelyworld.LivelyWorld;
 
 public class CreaturesModule {
 
 	private LivelyWorld plugin;
+	private BukkitRunnable runnable;
 
 	public void onEnable(LivelyWorld plugin) {
 		this.setPlugin(plugin);
+		this.runnable = new BukkitRunnable(){
+
+			@Override
+			public void run() {
+				for(World world : getPlugin().getServer().getWorlds()){
+					for(Chunk chunk : world.getLoadedChunks()){
+						Entity[] entities = chunk.getEntities();
+						for(Entity e : entities){
+							if(isAnimal(e)){
+								if(Math.random() >= 0.1) continue;
+								Ageable animal = (Ageable) e;
+								boolean ate = false;
+								if(isEdibleBlock(e.getLocation().getBlock())){
+									Block b = e.getLocation().getBlock();
+									if(isBreakableBlock(b)){
+										b.setType(Material.AIR);
+										b.getWorld().playSound(e.getLocation(), Sound.BLOCK_GRASS_BREAK, 1, 1);
+									} else {
+										b.setType(Material.DIRT);
+										b.getWorld().playSound(e.getLocation(), Sound.BLOCK_GRASS_BREAK, 1, 1);
+									}
+									ate = true;
+								} else if(isEdibleBlock(e.getLocation().getBlock().getRelative(BlockFace.DOWN))){
+									Block b = e.getLocation().getBlock().getRelative(BlockFace.DOWN);
+									if(isBreakableBlock(b)){
+										b.setType(Material.AIR);
+										b.getWorld().playSound(e.getLocation(), Sound.BLOCK_GRASS_BREAK, 1, 1);
+									} else {
+										b.setType(Material.DIRT);
+										b.getWorld().playSound(e.getLocation(), Sound.BLOCK_GRASS_BREAK, 1, 1);
+									}
+									ate = true;
+								}
+								if(ate == true){
+									double mxHealth = animal.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue();
+									if(animal.getHealth() >= mxHealth - 2 && animal.isAdult()){
+										animal.setBreed(true);
+									}
+									animal.setHealth(Math.min(animal.getHealth() + 1, mxHealth));
+								} else {
+									animal.damage(1);
+								}
+							}
+						}
+					}
+				}
+			}
+			
+		};
+		this.runnable.runTaskTimer(getPlugin(), 10, 20 * 5);
+	}
+	
+	public boolean isAnimal(Entity e) {
+		if(e.getType() == EntityType.COW) return true;
+		if(e.getType() == EntityType.SHEEP) return true;
+		return false;
+	}
+	
+	public boolean isEdibleBlock(Block block){
+		if(block.getType() == Material.GRASS) return true;
+		if(block.getType() == Material.LONG_GRASS) return true;
+		if(block.getType() == Material.DOUBLE_PLANT) return true;
+		if(block.getType() == Material.LEAVES) return true;
+		if(block.getType() == Material.LEAVES_2) return true;
+		if(block.getType() == Material.CROPS) return true;
+		return false;
+	}
+	
+	public boolean isBreakableBlock(Block block){
+		if(block.getType() == Material.LONG_GRASS) return true;
+		if(block.getType() == Material.DOUBLE_PLANT) return true;
+		if(block.getType() == Material.LEAVES) return true;
+		if(block.getType() == Material.LEAVES_2) return true;
+		if(block.getType() == Material.CROPS) return true;
+		return false;
 	}
 
 	public void onDisable() {
-
+		if(this.runnable != null){
+			this.runnable.cancel();
+		}
 	}
 
 	public void onBlockUpdate(Block b, Player p) {

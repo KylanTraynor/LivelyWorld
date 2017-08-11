91abf65771c34464ae4865fe2b226a9096b5c381
KylanTraynor <kylan2837@gmail.com>
2017-08-12 00:17:16 +0200

    Stuff.

--- LivelyWorld/src/main/java/com/kylantraynor/livelyworld/water/WaterListener.java
+++ LivelyWorld/src/main/java/com/kylantraynor/livelyworld/water/WaterListener.java
@@ -56,6 +56,7 @@ public class WaterListener implements Listener{
 	@EventHandler
 	public void onChunkLoad(ChunkLoadEvent event){
 		Chunk c = event.getChunk();
+		if(!c.getWorld().equals("world")) return;
 		WaterChunk wc = WaterChunk.get(c.getWorld(), c.getX(), c.getZ());
 		if(wc.isLoaded) return;
 		BukkitRunnable br = new BukkitRunnable(){
@@ -70,6 +71,7 @@ public class WaterListener implements Listener{
 	@EventHandler
 	public void onChunkUnload(ChunkUnloadEvent event){
 		Chunk c = event.getChunk();
+		if(!c.getWorld().equals("world")) return;
 		WaterChunk wc = WaterChunk.get(c.getWorld(), c.getX(), c.getZ());
 		BukkitRunnable br = new BukkitRunnable(){
 			@Override

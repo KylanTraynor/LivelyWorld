7d5ef4c2e3e6997ad1e9dfe21f2e5465cb0b5929
KylanTraynor <kylan2837@gmail.com>
2017-10-11 19:01:56 +0200

    Stuff

--- LivelyWorld/src/main/java/com/kylantraynor/livelyworld/water/WaterListener.java
+++ LivelyWorld/src/main/java/com/kylantraynor/livelyworld/water/WaterListener.java
@@ -47,9 +47,10 @@ public class WaterListener implements Listener{
 	
 	@EventHandler(ignoreCancelled = true)
 	public void onBlockFromTo(BlockFromToEvent event){
-		/*if(!Utils.isWater(event.getBlock())) return;
+		if(!Utils.isWater(event.getBlock())) return;
 		if(!event.getBlock().getWorld().getName().equals("world")) return;
-		event.setCancelled(true);*/
+		event.getBlock().setTypeIdAndData(Material.STATIONARY_WATER.getId(), event.getBlock().getData(), false);
+		event.setCancelled(true);
 		/*Biome fromBiome = event.getBlock().getBiome();
 		switch(fromBiome){
 		case OCEAN:

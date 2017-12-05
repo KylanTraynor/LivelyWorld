dc3f90936c1c0831481775db63fea2a679efa3af
KylanTraynor <kylan2837@gmail.com>
2017-12-05 18:33:00 +0100

    Stuff

--- LivelyWorld/Plugin/src/main/java/com/kylantraynor/livelyworld/climate/ClimateModule.java
+++ LivelyWorld/Plugin/src/main/java/com/kylantraynor/livelyworld/climate/ClimateModule.java
@@ -138,7 +138,7 @@ public class ClimateModule {
 											}
 										};
 										br.runTaskAsynchronously(getPlugin());
-										b.getWorld().spawnParticle(Particle.CLOUD, b.getLocation().add(Utils.fastRandomDouble(),Utils.fastRandomDouble(),Utils.fastRandomDouble()), evaporation, 0, 0, 0, 0.5);
+										b.getWorld().spawnParticle(Particle.CLOUD, b.getLocation().add(0.5,0.5,0.5), evaporation, 0.5, 0.5, 0.5, 0.25);
 										//b.getWorld().spawnParticle(Particle.SMOKE_LARGE, b.getLocation().add(0.5, 0.5, 0.5), evaporation, 0.5, 0.5, 0.5);
 									} else {
 										while((b.getRelative(BlockFace.DOWN).getType() == Material.AIR ||

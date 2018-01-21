c87325c5662d72f38027323a9fd721849f4e180d
KylanTraynor <kylan2837@gmail.com>
2018-01-21 16:11:00 +0100

    MoarStuff

--- LivelyWorld/Plugin/src/main/java/com/kylantraynor/livelyworld/water/WaterChunk.java
+++ LivelyWorld/Plugin/src/main/java/com/kylantraynor/livelyworld/water/WaterChunk.java
@@ -576,7 +576,7 @@ public class WaterChunk {
 			wasGenerated = true;
 		}
 		byte[][][] levels = new byte[16][256][16];
-		boolean[][][] hasChanged = new boolean[16][256][16];
+		//boolean[][][] hasChanged = new boolean[16][256][16];
 		for(int x = 0; x < 16; x++){
 			for(int y = 0; y < 256; y++){
 				for(int z = 0; z < 16; z++){
